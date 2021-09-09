32303fe910ca649b4d1f7da3e0f1f04b82d50814
thiagofandrade92@gmail.com <thiagofandrade92@gmail.com>
2021-09-08 22:34:31 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import NavBar from "components/NavBar";
+
 function App() {
   return (
     <div>
       <h1>"Olá Mundo"</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+      <div>
+        <h2>"Dev Superior"</h2>
+      </div>
+    );
+  }
+  export default NavBar;
+  
